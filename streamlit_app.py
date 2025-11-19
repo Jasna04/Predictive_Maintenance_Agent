4c52a59d62ae5fbef25f959014c5d07094eff004
Jasna04 <86100007+Jasna04@users.noreply.github.com>
2025-11-19 17:31:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ except:
 # -----------------------------
 # App Config
 # -----------------------------
-st.set_page_config(page_title="Predictive Maintenance (A2A Demo)", layout="wide")
+st.set_page_config(page_title="Predictive Maintenance Agent", layout="wide")
 SERPAPI_KEY = os.getenv("SERPAPI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
